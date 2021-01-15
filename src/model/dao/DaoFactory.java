1b5f228d32f747936060f73f2c191127f0e735a1
Manoel Luis <manoelluisjunior@hotmail.com>
2021-01-15 14:11:02 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return (SellerDao) new SellerDaoJDBC();
+	}
+	
+	
+		
+	}
+
+
